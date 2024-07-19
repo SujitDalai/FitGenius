29a37e9f4828c705ad0d173e149989beb94abff5
SujitDalai <sujit.dalai.20cse@bmu.edu.in>
2024-07-19 10:12:12 +0530

    Desc Nutrition Commit

--- src/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup.tsx
+++ src/components/ReportFormPopup/CalorieIntake/CalorieIntakePopup.tsx
@@ -27,6 +27,21 @@ const CalorieIntakePopup: React.FC<CaloriIntakePopupProps> = ({ setShowCalorieIn
     quantitytype: 'g'
   });
 
+              // body :[ {
+            //     "name": "rice",
+            //     "calories": 127.4,
+            //     "serving_size_g": 100,
+            //     "fat_total_g": 0.3,
+            //     "fat_saturated_g": 0.1,
+            //     "protein_g": 2.7,
+            //     "sodium_mg": 1,
+            //     "potassium_mg": 42,
+            //     "cholesterol_mg": 0,
+            //     "carbohydrates_total_g": 28.4,
+            //     "fiber_g": 0.4,
+            //     "sugar_g": 0.1
+            // }]
+
   const [items, setItems] = React.useState<any>([]);
 
   const saveCalorieIntake = async () => {
